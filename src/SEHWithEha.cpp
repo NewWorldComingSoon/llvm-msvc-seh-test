9be2b0a9739f7826c8d0fc954891dc60242250e7
gmh5225 <2315157@qq.com>
2023-07-19 19:02:43 +0800

    Update

--- src/SEHWithEha.cpp
+++ src/SEHWithEha.cpp
@@ -22,3 +22,34 @@ TEST(SEHWithEha, SEHWithEha_1)
 
     EXPECT_EQ(2, value);
 }
+
+TEST(SEHWithEha, SEHWithEha_2)
+{
+    int value = 0;
+    try
+    {
+        value += 1;
+        int *p = (int *)1;
+        *p = NULL;
+    }
+    catch (...)
+    {
+        value += 1;
+        printf("except!\n");
+    }
+
+    try
+    {
+        value += 1;
+        int *p = (int *)0;
+        *p = NULL;
+    }
+    catch (...)
+    {
+        value += 1;
+        printf("except2!\n");
+    }
+
+    EXPECT_EQ(4, value);
+}
+
