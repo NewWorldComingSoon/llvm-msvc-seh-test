b32bf48aa043fca2e79ed515d1853844ee5bd8e1
gmh5225 <2315157@qq.com>
2023-07-19 19:07:05 +0800

    Update

--- src/SEHWithEha.cpp
+++ src/SEHWithEha.cpp
@@ -79,3 +79,36 @@ TEST(SEHWithEha, SEHWithEha_3)
     }
     printf("unreachable\n");
 }
+
+TEST(SEHWithEha, SEHWithEha_4)
+{
+    int value = 0;
+    std::string s1 = "123213";
+    try
+    {
+        value += 1;
+        std::vector<int> a;
+        a.push_back(1);
+        a.push_back(2);
+        for (auto v : a)
+        {
+            printf("v:%d\n", v);
+        }
+
+        std::string s2 = "22222222";
+        printf("try s2=%s\n", s2.c_str());
+
+        int *p = (int *)1;
+        *p = NULL;
+        value += 1;
+    }
+    catch (...)
+    {
+        value += 1;
+        printf("except!\n");
+        printf("except! s1=%s\n", s1.c_str());
+        EXPECT_EQ(2, value);
+        return;
+    }
+    printf("unreachable\n");
+}
