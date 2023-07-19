c870135fcad16b0133b0a21917f2296c4ba2d82b
gmh5225 <2315157@qq.com>
2023-07-19 17:38:20 +0800

    Update

--- src/SEH.cpp
+++ src/SEH.cpp
@@ -39,3 +39,38 @@ TEST(SEH, SHE_1)
 
     EXPECT_EQ(5, value);
 }
+
+TEST(SEH, SHE_2)
+{
+    int value = 0;
+
+    __try
+    {
+        value += 1;
+        printf("try start\n");
+        _asm {int 3}
+        printf("try failed\n");
+    }
+    __except (1)
+    {
+        value += 1;
+        printf("except seh 1\n");
+    }
+
+    value += 1;
+
+    __try
+    {
+        value += 1;
+        printf("try start2\n");
+        _asm {int 3}
+        printf("try failed2\n");
+    }
+    __except (1)
+    {
+        value += 1;
+        printf("except seh 2\n");
+    }
+
+    EXPECT_EQ(5, value);
+}
