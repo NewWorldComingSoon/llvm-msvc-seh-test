3f0d43b064aead1a48b85e2d295dc6326f159de0
gmh5225 <2315157@qq.com>
2023-07-19 17:24:34 +0800

    Update

---
+++ src/CXXEH.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+#include <Windows.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
---
+++ src/SEH.cpp
@@ -0,0 +1,41 @@
+#include <gtest/gtest.h>
+#include <Windows.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+TEST(SEH, SHE_1)
+{
+    int value = 0;
+
+    __try
+    {
+        printf("try start\n");
+        value += 1;
+        __debugbreak();
+        printf("try failed\n");
+    }
+    __except (1)
+    {
+        value += 1;
+        printf("except seh 1\n");
+    }
+
+    value += 1;
+
+    __try
+    {
+        value += 1;
+        printf("try start2\n");
+        __debugbreak();
+        printf("try failed2\n");
+    }
+    __except (1)
+    {
+        value += 1;
+        printf("except seh 2\n");
+    }
+
+    EXPECT_EQ(5, value);
+}
