62e310c06cc5fc2c8c3ceb62f681a13a2ade1d87
gmh5225 <2315157@qq.com>
2023-07-20 16:14:12 +0800

    Update CXXEH.cpp

--- src/CXXEH.cpp
+++ src/CXXEH.cpp
@@ -29,3 +29,39 @@ TEST(CXXEH, CXXEH_1)
         EXPECT_STREQ("heihei", e.what());
     }
 }
+
+void
+CXXEH_2_parse(const std::string &fmt, const std::string &str)
+{
+    std::string allstr = fmt + str;
+    if (allstr.compare("1234") == 0)
+    {
+        throw std::exception("heihei");
+    }
+    printf("a+b=%s\n", allstr.c_str());
+    return;
+}
+
+void
+CXXEH_2_tryParse(const std::string &fmt, const std::string &str)
+{
+    try
+    {
+        printf("start parse\n");
+        CXXEH_2_parse(fmt, str);
+    }
+    catch (const std::exception &e)
+    {
+        printf("except=%s\n", e.what());
+        EXPECT_STREQ("heihei", e.what());
+    }
+
+    printf("end tryParse\n");
+}
+
+TEST(CXXEH, CXXEH_2)
+{
+    std::string fmt = "12";
+    std::string str = "34";
+    CXXEH_2_tryParse(fmt, str);
+}
