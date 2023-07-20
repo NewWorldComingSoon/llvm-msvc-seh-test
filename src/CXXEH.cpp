bf889f46fd9700906b0793513eca6abc4b8bdc9b
gmh5225 <2315157@qq.com>
2023-07-20 16:05:24 +0800

    Update CXXEH.cpp

--- src/CXXEH.cpp
+++ src/CXXEH.cpp
@@ -4,3 +4,28 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
+
+void
+CXXEH_1_parse(int a, int b)
+{
+    if (a + b == 4)
+    {
+        throw std::exception("heihei");
+    }
+    printf("a+b=%d\n", a + b);
+    return;
+}
+
+TEST(CXXEH, CXXEH_1)
+{
+    try
+    {
+        printf("start parse\n");
+        CXXEH_1_parse(1, 3);
+    }
+    catch (const std::exception &e)
+    {
+        printf("except=%s\n", e.what());
+        EXPECT_STREQ("heihei", e.what());
+    }
+}
